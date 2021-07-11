6300c89846550f3498becf866b3872b3d9b76039
SeanCassone <sean.cassone@gmail.com>
2021-07-10 23:45:02 -0400

    basic navbar complete

--- src/components/Navagation/index.js
+++ src/components/Navagation/index.js
@@ -1,4 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
 const Navagation = () => {
-  return <h1>Navagation</h1>;
+  return (
+    <>
+      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/projects">Projects</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 export default Navagation;
